a0e3f883263e4e53e0e01b55d0e147f245ee352d
Gavin <gavinporlich@gmail.com>
2023-01-20 01:51:40 -0600

    Added loginUser function/connected routes/protected getRoutes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,25 @@
 const User = require("../models/User");
-const { signToken } = require("../utils/auth");
+const { authMiddleware, signToken } = require("../utils/auth");
 const gravatar = require('gravatar')
 
 async function getAllUsers(req, res) {
-    try {
+  try {
       const allUsers = await User.find()
         .select("-__v")
         .select("-password")
       res.status(200).json(allUsers);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  }
+
+  async function getUserById(req, res) {
+    try {
+      const singleUser = await User.findById(req.params.userId)
+        .select("-__v")
+        .select("-password")
+      res.status(200).json(singleUser);
     } catch (err) {
       console.error(err);
       res.status(500).json(err);
@@ -37,7 +49,40 @@ async function createUser(req, res) {
     };
   };
 
+  async function loginUser(req, res) {
+    try {
+      const user = await User.findOne({
+        email: req.body.email,
+      }).select("-__v");
+  
+      if (!user) {
+        res
+          .status(400)
+          .json({ message: "Incorrect email or password, please try again" });
+        return;
+      }
+  
+      const validPassword = await user.isCorrectPassword(req.body.password);
+      console.log(validPassword)
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: "Incorrect email or password, please try again" });
+        return;
+      }
+  
+      const token = signToken(user);
+      res.json({ token, user });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  }
+  
+
   module.exports = {
     getAllUsers,
-    createUser
+    getUserById,
+    createUser,
+    loginUser,
   }
--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
+const { authMiddleware } = require("../../utils/auth");
 const {
-    getAllUsers,
+  getAllUsers,
   createUser,
+  getUserById,
+  loginUser,
 } = require("../../controllers/userController");
 
 // /api/users
-router.route("/").get(getAllUsers).post(createUser);
+router.route("/").get(authMiddleware, getAllUsers).post(createUser);
+router.route("/:id").get(authMiddleware, getUserById);
+router.route("/login").post(loginUser);
 
 // /api/users/:userId
 // router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
