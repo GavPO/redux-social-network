ce9b22323518a17ba7712968e491be07c49fc765
Gavin <gavinporlich@gmail.com>
2023-01-20 03:51:40 -0600

    Post model created

---
+++ server/models/Post.js
@@ -0,0 +1,44 @@
+const { Schema, model, Types } = require("mongoose");
+
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = model("Post", postSchema);
+
+module.exports = Post;
